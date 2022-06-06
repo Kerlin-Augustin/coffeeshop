53795fb62ffe325c8ca6b1abf3406a3858687591
Kerlin Augustin <kerlin.augustin1@gmail.com>
2022-06-05 20:50:44 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://kerlinaugustin:sneakyboi7@coffeeshop.w2thu.mongodb.net/coffeeshop?retryWrites=true&w=majority",
+    'db' : "coffeeshop"
+};
